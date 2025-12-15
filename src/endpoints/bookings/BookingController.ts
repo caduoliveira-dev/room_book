eef307f4704a857fd0b9075ae3d2a43bc8f85f54
cadu <ribeirocarlosoliveira@gmail.com>
2025-12-15 20:01:29 -0300

    add endpoints /get para rota bookings

--- src/endpoints/bookings/BookingController.ts
+++ src/endpoints/bookings/BookingController.ts
@@ -4,24 +4,60 @@ import { BookingRepository } from "./BookingRepository";
 import { BookingService } from "./BookingService";
 import { betterAuth } from "../../macros/auth";
 
-export const bookingController = new Elysia().use(betterAuth).post(
-  "/bookings",
-  async ({ body, user }) => {
-    const service = new BookingService(new BookingRepository());
-    return await service.createBooking({
-      ...body,
-      userId: user.id,
-    });
-  },
-  {
-    auth: true,
-    body: createBookingSchema.omit({ userId: true }),
-    response: {
-      201: bookingResponseSchema,
+export const bookingController = new Elysia()
+  .use(betterAuth)
+  .post(
+    "/bookings",
+    async ({ body, user }) => {
+      const service = new BookingService(new BookingRepository());
+      return await service.createBooking({
+        ...body,
+        userId: user.id,
+      });
     },
-    detail: {
-      summary: "Create a new booking",
-      tags: ["Bookings"],
+    {
+      auth: true,
+      body: createBookingSchema.omit({ userId: true }),
+      response: {
+        201: bookingResponseSchema,
+      },
+      detail: {
+        summary: "Create a new booking",
+        tags: ["Bookings"],
+      },
+    }
+  )
+  .get(
+    "/bookings",
+    async () => {
+      const service = new BookingService(new BookingRepository());
+      return await service.getAllBookings();
     },
-  }
-);
+    {
+      auth: true,
+      response: {
+        200: bookingResponseSchema.array(),
+      },
+      detail: {
+        summary: "Get all bookings",
+        tags: ["Bookings"],
+      },
+    }
+  )
+  .get(
+    "/bookings/:id",
+    async ({ params }) => {
+      const service = new BookingService(new BookingRepository());
+      return await service.getBookingById(params.id);
+    },
+    {
+      auth: true,
+      response: {
+        200: bookingResponseSchema,
+      },
+      detail: {
+        summary: "Get a booking by ID",
+        tags: ["Bookings"],
+      },
+    }
+  );
